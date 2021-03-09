d6bf1871021df88416b027e2e795eb0789e13601
andrew <isbushcar@gmail.com>
2021-03-09 11:10:14 +1000

    game-even refactoring

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -15,17 +15,13 @@ def is_even(number):
     return 'no'
 
 
-def brain_game_even():
+def brain_game_even(wins_number):
     """Interact with user and return win or lose."""
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    wins_number = 0
-    while wins_number < 3:
-        random_num = random.randint(0, 100)
-        print('Question: {0}'.format(random_num))
-        answer = prompt.string('Your answer: ')
-        if answer == is_even(random_num):
-            print('Correct!')
-            wins_number += 1
-        else:
-            return (answer, is_even(random_num), 'lose')
-    return 'win'
+    if wins_number == 0:
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+    random_num = random.randint(0, 100)
+    print('Question: {0}'.format(random_num))
+    answer = prompt.string('Your answer: ')
+    if answer == is_even(random_num):
+        return 'win'
+    return (answer, is_even(random_num), 'lose')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,12 +4,10 @@
 """Brain-even game."""
 
 
-from brain_games.game_engine import game_end, game_start
+from brain_games.game_engine import main as game_engine
 from brain_games.games.game_even import brain_game_even
 
 
 def main():
     """Made to run a brain-even game."""
-    name = game_start()
-    game_result = brain_game_even()
-    game_end(game_result, name)
+    game_engine(brain_game_even)
