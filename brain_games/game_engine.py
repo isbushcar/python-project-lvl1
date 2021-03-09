2fc94bc1309349887557274ef5db58bf263593de
andrew <isbushcar@gmail.com>
2021-03-09 12:14:20 +1000

    engine fixes

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -2,7 +2,6 @@
 
 
 import prompt
-import brain_games.games
 
 
 def game_start():
@@ -15,21 +14,22 @@ def game_end(game_result, name):
     """Made to get game result and user's name and return final message."""
     if game_result == 'win':
         print('Congratulations, {0}!'.format(name))
-    if game_result[2] == 'lose':
+    if game_result[0] == 'lose':
         message = ' is wrong answer ;(. Correct answer was '
-        print("'{0}'{1}'{2}'.".format(game_result[0], message, game_result[1]))
+        print("'{0}'{1}'{2}'.".format(game_result[1], message, game_result[2]))
         print("Let's try again, {0}!".format(name))
 
 
 def main(game_name):
+    """Game engine."""
     name = game_start()
     wins_number = 0
     while wins_number < 3:
-        (users_answer, correct_answer, round_result) = game_name()
+        round_result = game_name(wins_number)
         if round_result == 'win':
             print('Correct!')
             wins_number += 1
         else:
-            game_end(users_answer, correct_answer, 'lose')
+            return game_end(round_result, name)
     if wins_number == 3:
-        game_end('win')
+        game_end('win', name)
