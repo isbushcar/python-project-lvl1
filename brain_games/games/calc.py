8c7afaa022a5c8d85aabd93a492923c5f4001b1c
andrew <isbushcar@gmail.com>
2021-03-10 13:30:16 +1000

    minor calc-game refacroting

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,10 +3,8 @@
 
 import random
 
-import prompt
 
-
-def expression_generator():
+def play_calc():
     """Generate expression, return string with it and its result."""
     operator_number = random.randint(1, 3)
     if operator_number == 1:
@@ -14,28 +12,21 @@ def expression_generator():
         number_b = random.randint(0, 100)
         expression_result = number_a + number_b
         expression_string = '{0} + {1}'.format(str(number_a), str(number_b))
-        return (expression_string, expression_result)
+        return expression_string, expression_result
     if operator_number == 2:
         number_a = random.randint(0, 100)
         number_b = random.randint(0, number_a)
         expression_result = number_a - number_b
         expression_string = '{0} - {1}'.format(str(number_a), str(number_b))
-        return (expression_string, expression_result)
+        return expression_string, expression_result
     if operator_number == 3:
         number_a = random.randint(0, 100)
         number_b = random.randint(0, 10)
         expression_result = number_a * number_b
         expression_string = '{0} * {1}'.format(str(number_a), str(number_b))
-        return (expression_string, expression_result)
+        return expression_string, expression_result
 
 
-def brain_game_calc(wins_number):
-    """Interacts with user and return win or lose."""
-    if wins_number == 0:
-        print('What is the result of the expression?')
-    (expression_string, expression_result) = expression_generator()
-    print('Question: {0}'.format(expression_string))
-    answer = prompt.integer('Your answer: ')
-    if answer == expression_result:
-        return 'win'
-    return 'lose', answer, expression_result
+def brief_calc():
+    """Made to return game briefing."""
+    return 'What is the result of the expression?'
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -5,9 +5,9 @@
 
 
 from brain_games.game_engine import main as game_engine
-from brain_games.games.calc import brain_game_calc
+from brain_games.games.calc import brief_calc, play_calc
 
 
 def main():
     """Made to run a brain-calc game."""
-    game_engine(brain_game_calc)
+    game_engine(play_calc, brief_calc)
