dd0a18149e2336fb0fa8af1fb52183982adcb58d
andrew <isbushcar@gmail.com>
2021-03-10 13:29:41 +1000

    minor game_engine fixes

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -4,11 +4,6 @@
 import prompt
 
 
-def get_name():
-    """Asks and returns user's name."""
-    return prompt.string('May I have your name? ')
-
-
 def show_game_result(game_result, name):
     """Made to get game result and user's name and return final message."""
     if game_result == 'win':
@@ -22,18 +17,18 @@ def show_game_result(game_result, name):
 def main(game_name, brief_name):
     """Game engine."""
     print('Welcome to the Brain Games!')
-    name = get_name()
+    name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
     print(brief_name())
     wins_number = 0
     while wins_number < 3:
         (question, correct_answer) = game_name()
         print('Question: {0}'.format(question))
-        users_answer = prompt.string('Your answer: ')
-        if users_answer == str(correct_answer):
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == str(correct_answer):
             print('Correct!')
             wins_number += 1
         else:
-            return show_game_result(('lose', users_answer, correct_answer), name)
+            return show_game_result(('lose', user_answer, correct_answer), name)
     if wins_number == 3:
         show_game_result('win', name)
