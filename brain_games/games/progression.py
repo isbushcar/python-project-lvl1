cd1378923b2ace0f72e544be05d34f4b776ea62a
andrew <isbushcar@gmail.com>
2021-03-11 13:27:46 +1000

    progression-game refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,6 +1,5 @@
 """Contains brain-progression game."""
 
-
 import random
 
 BRIEFING = 'What number is missing in the progression?'
@@ -8,11 +7,14 @@ BRIEFING = 'What number is missing in the progression?'
 
 def play():
     """Made to generate progression, hide one number and return both."""
-    progression = [str(random.randint(1, 50)),]
+    progression = [str(random.randint(1, 100))]
     progression_step = random.randint(1, 10)
     changed_progression = ' '
     for index in range(0, random.randint(5, 10)):
         progression.append(str(int(progression[index]) + progression_step))
     hidden_number = random.randint(0, len(progression) - 1)
-    progression[hidden_number], hidden_number = '..', progression[hidden_number]
+    hidden_number, _ = (
+        progression.pop(hidden_number),
+        progression.insert(hidden_number, '..'),
+    )
     return changed_progression.join(progression), hidden_number
