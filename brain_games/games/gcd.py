cb89fffcd91d07c3396be781e3cbbe4795c8eddd
andrew <isbushcar@gmail.com>
2021-03-11 10:56:37 +1000

    minor gcd-game refactoring

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,8 +9,8 @@ BRIEFING = 'Find the greatest common divisor of given numbers.'
 def play():
     """Generate two numbers, return them with their greatest common divisor."""
     num_one = random.randint(1, 100)
-    num_two = random.randint(1, num_one)
-    divisor = num_two
+    num_two = random.randint(1, 100)
+    divisor = min(num_one, num_two)
     while divisor > 1:
         if num_one % divisor == 0 and num_two % divisor == 0:
             question = [num_one, num_two]
