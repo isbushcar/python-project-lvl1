4e4581be95e302856100ca70eea9b26209edce0d
berezin <isbushcar@gmail.com>
2021-03-15 20:18:20 +1000

    minor stage-4 refactoring

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -3,15 +3,15 @@
 import prompt
 
 
-def run_game(game_name):
+def run_game(game):
     """Game engine. Runs game and returns it's result."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
-    print(game_name.BRIEFING)
+    print(game.BRIEFING)
     wins_number = 0
     while wins_number < 3:
-        question, correct_answer = game_name.generate_question_and_answer()
+        question, correct_answer = game.generate_question_and_answer()
         print('Question: {0}'.format(question))
         user_answer = prompt.string('Your answer: ')
         if user_answer == str(correct_answer):
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,13 +5,15 @@ import random
 BRIEFING = 'What number is missing in the progression?'
 
 
-def generate_question_and_answer():
+def generate_question_and_answer():  # noqa: WPS210
     """Made to generate progression, hide one number and return both."""
     progression = []
-    start_number = str(random.randint(1, 100))
+    start_number = random.randint(1, 100)
     progression_step = random.randint(1, 10)
     for index in range(random.randint(5, 10)):
-        progression.append(str(int(start_number) + progression_step * index))
-    hidden_number = progression[random.randint(0, len(progression) - 1)]
-    progression[progression.index(hidden_number)] = '..'
+        progression.append(start_number + progression_step * index)
+    progression = [str(number) for number in progression]
+    hidden_number_index = random.randint(0, len(progression) - 1)
+    hidden_number = progression[hidden_number_index]
+    progression[hidden_number_index] = '..'
     return ' '.join(progression), hidden_number
