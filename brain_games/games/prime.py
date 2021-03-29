192ad8bd930f7b4c6ec3b0c394a630d03ce4090e
andrew <isbushcar@gmail.com>
2021-03-30 09:40:47 +1000

    brain-prime abstr refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,11 +9,16 @@ BRIEFING = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def generate_question_and_answer():
     """Made to return number and 'yes' if it is prime or 'no' if it is not."""
     number = random.randint(0, 100)
+    return (number, 'yes') if is_prime(number) else (number, 'no')
+
+
+def is_prime(number):
+    """Made to return True if number is prime or False if it is not."""
     if number <= 1:
-        return number, 'no'
+        return False
     divisor = number // 2
     while divisor > 1:
         if number % divisor == 0:
-            return number, 'no'
+            return False
         divisor -= 1
-    return number, 'yes'
+    return True
