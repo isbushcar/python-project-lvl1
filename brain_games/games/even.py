10113d0b5bb4069790275c35a5dca9a749f9e54d
andrew <isbushcar@gmail.com>
2021-03-30 09:34:44 +1000

    brain-even abstr refactoring

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,9 +6,12 @@ import random
 BRIEFING = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    """Made to return True if number is even and False if it is not."""
+    return number % 2 == 0
+
+
 def generate_question_and_answer():
     """Made to return question and correct answer."""
     number = random.randint(0, 100)
-    if number == 0 or number % 2 == 0:
-        return number, 'yes'
-    return number, 'no'
+    return (number, 'yes') if is_even(number) else (number, 'no')
